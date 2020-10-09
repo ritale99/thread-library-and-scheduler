9e1c80237432eebf537a85c40e6624530702803e
MDotSan <71162128+MDotSan@users.noreply.github.com>
2020-10-09 16:20:52 -0400

    Update mypthread.c
    Fix errors preventing compilation
    Fix sigsegv problem
    
    Added a way of getting fresh thread id (however, this is getting only the current highest thread id + 1)
    Added a way to remove thread from the master queue using node of the associated thread

--- mypthread.c
+++ mypthread.c
@@ -13,7 +13,21 @@
 Queue * master_queue;
 
 //main context
+const static mypthread_t main_thread_id = 0;
+static tcb main_tcb;
 static ucontext_t main_context;
+
+//scheduler
+static Schedule_Policy sched;
+
+//current running thread
+mypthread_t curr_thread_id;
+
+/* this function is called when creating new thread */
+static void thread_helper (void * (*function) (void*), void * arg){
+	printf("Ended here\n");	//debug
+	mypthread_exit(function(arg));
+}
 
 /* create a new thread */
 int mypthread_create(mypthread_t * thread, pthread_attr_t * attr,
@@ -27,33 +41,36 @@ int mypthread_create(mypthread_t * thread, pthread_attr_t * attr,
 	//if master queue is empty
 	//we create a new master queue,
 	//and the first level queue as well
-	if ( master_queue == NULL ) master_queue = (Queue *) malloc (sizeof(Queue));
-	if ( isEmpty (master_queue) ){
-		Queue * queue_1 = (Queue*)malloc (sizeof(Queue));
-		Node * node = (Node *) malloc (sizeof(Node));
-		node->data = queue_1;
-		Enqueue(master_queue, node);
+	if ( master_queue == NULL )
+		master_queue = CreateQueue();
+	if ( master_queue == NULL ) exit(1);
+	if ( isempty (master_queue) ){
+		Queue * queue_0 = CreateQueue();
+		Node * queue_node = (Node *) malloc (sizeof(Node));
+		queue_node->data = (void*) queue_0;
+		Enqueue(master_queue, queue_node);
 	}
-
-	//We need to figure out based on the scheduler which queue level to send this thread too
-	//right now I'm assuming the first level
 
 	//initialization
 	Node * thread_node = NULL;
-	char thread_stack [] = NULL;
+	char * thread_stack = NULL;
 	tcb * tcb_ptr = NULL;
-
-	//do we have to create a pthread?
+	//currently we dont have a way to generate a fresh id
+	//TODO: generate fresh id
+	//mypthread_t thread_id = FreshThreadID();
+	mypthread_t thread_id = 1;
 
 	//allocate the stack based on the SIGSTKSZ size
-	thread_stack = malloc(SIGSTKSZ);
+	thread_stack = (char *)malloc(SIGSTKSZ);
 	if ( thread_stack == NULL ) return -1;
+
 	//allocate space for tcb
-	tcb_ptr = (tcb) malloc(sizeof(tcb));
+	tcb_ptr = (tcb*) malloc(sizeof(tcb));
 	if ( tcb_ptr == NULL ){
 		free(thread_stack);
 		return -1;
 	}
+
 	//allocate space for the thread_node
 	thread_node = (Node *) malloc (sizeof(Node));
 	if ( thread_node == NULL ) {
@@ -62,23 +79,32 @@ int mypthread_create(mypthread_t * thread, pthread_attr_t * attr,
 		return -1;
 	}
 
-	//assign thread_id
-	tcb_ptr->thread_id = 0;
-
 	//assign thread_state to ready state
 	tcb_ptr->thread_state = Ready;
 
-	//assign thread_t
-	tcb_ptr->thread_t = thread;
+	//assign thread_id
+	tcb_ptr->thread_id = thread_id;
 
-	//create thread context
+	//assign thread_stack to tcb
+	tcb_ptr->thread_stack = thread_stack;
+
+	if ( thread != NULL ) (*thread) = thread_id;
+
+	//assign thread context
 	ucontext_t * thread_context_ptr = &(tcb_ptr->thread_context);
 
 	//assign current context to thread_context_ptr
 	if ( getcontext(thread_context_ptr) == -1 ) {
 		//we have received an error in an attempt
 		//to get the thread
+		free(thread_node->next);
+		free(thread_node);
+
 		free(thread_stack);
+
+		free(tcb_ptr->joined_val);
+		free(tcb_ptr->return_val);
+		free(tcb_ptr->thread_stack);
 		free(tcb_ptr);
 		return -1;
 	}
@@ -86,23 +112,28 @@ int mypthread_create(mypthread_t * thread, pthread_attr_t * attr,
 	//define the successor context when this context is done
 	//it should be the main context
 	thread_context_ptr->uc_link = &main_context;
-	//set the thread stack
 	thread_context_ptr->uc_stack.ss_sp = thread_stack;
-	thread_context_ptr->uc_stack.ss_size = sizeof(thread_stack);
-
-	makecontext( thread_context_ptr, function, arg ) ;
+	thread_context_ptr->uc_stack.ss_size = SIGSTKSZ;
 
 	//TODO: other things are needed to complete this function
 
-	//currently let the thread_node data be the TCB
-	thread_node->data = tcb_ptr;
+	thread_node->data = (void*)tcb_ptr;
+
+	//printf("%d\n", ((Queue *) (master_queue->front->data)) == NULL);
 
 	//enqueue to the front node (run node)
-	Enqueue(master_queue->front, thread_node);
+	Enqueue((Queue *) (master_queue->front->data), thread_node);
 
-	//scheduler
+	printf("thread: %d starting thread\n", thread_id);
+	curr_thread_id = thread_id;
 
-    return tcb_ptr->thread_id;
+	makecontext( thread_context_ptr, (void(*) (void))thread_helper, 2, function, arg ) ;
+
+	//we still need a scheduler, and not just swap to the thread_context_ptr
+	
+	swapcontext(&main_context, thread_context_ptr);
+
+    return 0;
 };
 
 /* give CPU possession to other user-level threads voluntarily */
@@ -110,26 +141,38 @@ int mypthread_yield() {
 
 	// change thread state from Running to Ready
 	// save context of this thread to its thread control block
-	// with from thread context to scheduler context
+	// switch from thread context to scheduler context
 
 	if ( master_queue == NULL ) return -1;
-	if ( main_context == NULL ) return -1;
-	//get current context
-	//swap context with main
-	//put thread in ready state
+	if ( &main_context == NULL ) return -1;
 
-	Node * curr_tcb_node = NULL;
-	ucontext_t * curr_context = NULL;
+	Node * curr_node = NULL;
+	tcb * curr_tcb_node = NULL;
+	ucontext_t * prev_context = NULL;
+	mypthread_t prev_thread_id;
 
-	if ( getcontext(curr_context) == -1 ) return -1;
-	if ( curr_context == NULL ) return -1;
+	//assign the current context to the previous context
+	//because we will be changing it
+	if(getcontext(prev_context) == -1) {
+		printf("(132) Error, attempted to get context but failed\n");
+		return -1;
+	}
+	//whichever thread called this is the the
+	//thread with curr_thread_id
+	prev_thread_id = curr_thread_id;
+	curr_node = GetNode(prev_thread_id);
+	if ( curr_node == NULL ) {
+		printf("(139) Error, could not find the node: %d\n", prev_thread_id);
+		return -1;
+	}
+	curr_tcb_node = (tcb*) curr_node;
+	curr_tcb_node->thread_context = (*prev_context);
+	//switch curr_thread_id to the main_thread_id
+	curr_thread_id = main_thread_id;
 
-	curr_tcb_node = GetNode(master_queue, curr_context);
+	//do we switch to main_context?
+	swapcontext(prev_context, &main_context);
 
-	if ( curr_tcb_node == NULL
-			|| ((tcb *) curr_tcb_node)->) return -1;
-
-	// YOUR CODE HERE
 	return 0;
 };
 
@@ -138,6 +181,45 @@ void mypthread_exit(void *value_ptr) {
 	// Deallocated any dynamic memory created when starting this thread
 
 	// YOUR CODE HERE
+	if ( master_queue == NULL ) return;
+
+	Node * curr_node = NULL;
+	tcb * curr_tcb_ptr = NULL;
+	ucontext_t prev_context;
+	mypthread_t prev_thread_id;
+
+	printf("Called\n");
+	prev_thread_id = curr_thread_id;
+
+	printf("Called 2 %d\n", prev_thread_id);
+
+	curr_node = GetNode(prev_thread_id);
+
+	printf("Called 3\n");
+
+	if ( curr_node == NULL ) {
+		printf("(188) Error, could not find the node: %d\n", prev_thread_id);
+		return;
+	}
+
+	curr_tcb_ptr = (tcb*) (curr_node->data);
+	curr_tcb_ptr->thread_state = Done;
+	printf("Called 4\n");
+	//assign the main context the value_ptr of this context
+	if ( value_ptr != NULL ) main_tcb.joined_val = &value_ptr;	//do we use the joined_val?
+
+	//remove from the queue
+	//DequeueNode(master_queue, curr_node);
+	mypthread_DequeueNode(curr_node);
+	printf("Called 5\n");
+	free(curr_tcb_ptr->return_val);
+	free(curr_tcb_ptr->joined_val);
+	free(curr_tcb_ptr->thread_stack);
+	free(curr_tcb_ptr);
+	printf("Called 6\n");
+	curr_thread_id = main_thread_id;
+	printf("thread: %d\n", curr_thread_id);
+	//call scheduler?
 };
 
 
@@ -205,11 +287,12 @@ static void schedule() {
 
 	// YOUR CODE HERE
 
+
 // schedule policy
 #ifndef MLFQ
-	// Choose STCF
+	//sched_stcf();
 #else
-	// Choose MLFQ
+	//sched_mlfq();
 #endif
 
 }
@@ -218,6 +301,9 @@ static void schedule() {
 static void sched_stcf() {
 	// Your own implementation of STCF
 	// (feel free to modify arguments and return types)
+
+	if ( master_queue == NULL ) return;
+
 
 	// YOUR CODE HERE
 }
@@ -234,55 +320,131 @@ static void sched_mlfq() {
 
 // YOUR CODE HERE
 
-//Get the node associated with the given threadID
-Node * GetNode(Queue * master_queue, int threadID){
-	if (master_queue == NULL) return NULL;
-	if (isempty(master_queue)) return NULL;
-	//get the first queue from the master_queue
-	Node * queue_ptr = master_queue->front;
-	//loop through each queue level
-	while ( queue_ptr != NULL ){
-		if (queue_ptr->data != NULL) {
-			//get the first thread node
-			Node * thread_ptr = ((Queue*)queue_ptr->data)->front;
-			//loop through each thread node
-			while ( thread_ptr != NULL ) {
-				//check if thread node has the matching threadID
-				if ( thread_ptr->data != NULL &&
-						((tcb *)thread_ptr->data)->thread_id == threadID) {
-					return thread_ptr;
-				}
-				thread_ptr = thread_ptr->next;
-			}
+//get a new mypthread_t
+mypthread_t FreshThreadID(){
+	if ( master_queue == NULL ) return 0;
+	if ( isempty(master_queue) ) return 0;
+
+	//start at zero, then find the highest thread_id
+	//I don't know how else to do it
+
+	mypthread_t highest_threadID = main_thread_id;
+	Node * queue_level_node = master_queue->front;
+	Node * thread_node = NULL;
+	Queue * inner_queue = NULL;
+	tcb * tcb_ptr = NULL;
+	while(queue_level_node != NULL){
+		if ( queue_level_node->data == NULL) {
+			queue_level_node = queue_level_node->next;
+			continue;
 		}
-		queue_ptr = queue_ptr->next;
+		inner_queue = (Queue*)(queue_level_node->data);
+		thread_node = inner_queue->front;
+		while ( thread_node != NULL ) {
+			//error checking
+			if ( thread_node->data == NULL ) {
+				thread_node = thread_node->next;
+				continue;
+			}
+			tcb_ptr = (tcb*) (thread_node->data);
+
+			if (tcb_ptr->thread_id > highest_threadID)
+				highest_threadID = tcb_ptr->thread_id;
+
+			thread_node = thread_node->next;
+		}
+		queue_level_node = queue_level_node->next;
 	}
+	return highest_threadID + 1;
 }
 
-//Get the tcb node based on the threadContext
-Node * GetNode ( Queue * master_queue , ucontext_t * threadContext ){
+//Get the node associated with the given threadID
+Node * GetNode(mypthread_t threadID){
 	if (master_queue == NULL) return NULL;
 	if (isempty(master_queue)) return NULL;
-	//get the first queue from the master_queue
-	Node * queue_ptr = master_queue->front;
-	//loop through each queue level
-	while ( queue_ptr != NULL ){
-		if (queue_ptr->data != NULL) {
-			//get the first thread node
-			Node * thread_ptr = ((Queue*)queue_ptr->data)->front;
-			//loop through each thread node
-			while ( thread_ptr != NULL ) {
-				//check if thread node has the matching threadID
-				if ( thread_ptr->data != NULL &&
-						((tcb *)thread_ptr->data)->thread_context == *threadContext) {
-						return thread_ptr;
-				}
-				thread_ptr = thread_ptr->next;
-			}
+
+	Node * thread_node = NULL;
+	Queue * inner_queue = FindQueueContainingThreadID(threadID);
+
+	if ( inner_queue == NULL ) return NULL;
+
+	thread_node = inner_queue->front;
+	//loop through each thread node
+	while ( thread_node != NULL ) {
+		//check if thread node has the matching threadID
+		if ( thread_node->data != NULL &&
+				((tcb *)thread_node->data)->thread_id == threadID) {
+			return thread_node;
 		}
-		queue_ptr = queue_ptr->next;
+		thread_node = thread_node->next;
 	}
+	return NULL;
 }
+
+int mypthread_DequeueNode ( Node * node ){
+	if (master_queue == NULL) return -1;
+	if (isempty(master_queue)) return -1;
+	Queue * inner_queue = FindQueueContainingNode(node);
+	if (inner_queue == NULL) return -1;
+	return DequeueNode(inner_queue, node);
+}
+
+Queue * FindQueueContainingNode (Node * node){
+	if (master_queue == NULL) return NULL;
+	if (isempty(master_queue)) return NULL;
+	Node * queue_level_node = master_queue->front;
+	Node * thread_node = NULL;
+	Queue * inner_queue = NULL;
+	//loop through each queue level
+	while ( queue_level_node != NULL ){
+		if ( queue_level_node->data == NULL) {
+			queue_level_node = queue_level_node->next;
+			continue;
+		}
+	//get the first thread node
+	inner_queue = (Queue*)(queue_level_node->data);
+	thread_node = inner_queue->front;
+	//loop through each thread node
+		while ( thread_node != NULL ) {
+		//check if thread node has the matching threadID
+			if ( thread_node == node) {
+				return inner_queue;
+			}
+			thread_node = thread_node->next;
+		}
+		queue_level_node = queue_level_node->next;
+	}
+	return NULL;
+}
+Queue * FindQueueContainingThreadID (mypthread_t threadID){
+	if (master_queue == NULL) return NULL;
+	if (isempty(master_queue)) return NULL;
+	Node * queue_level_node = master_queue->front;
+	Node * thread_node = NULL;
+	Queue * inner_queue = NULL;
+	//loop through each queue level
+	while ( queue_level_node != NULL ){
+		if ( queue_level_node->data == NULL) {
+			queue_level_node = queue_level_node->next;
+			continue;
+		}
+		//get the first thread node
+		inner_queue = (Queue*)(queue_level_node->data);
+		thread_node = inner_queue->front;
+		//loop through each thread node
+		while ( thread_node != NULL ) {
+			//check if thread node has the matching threadID
+			if ( thread_node->data != NULL &&
+					((tcb *)thread_node->data)->thread_id == threadID) {
+				return inner_queue;
+			}
+			thread_node = thread_node->next;
+		}
+		queue_level_node = queue_level_node->next;
+	}
+	return NULL;
+}
+
 //Generic Queue
 Queue * CreateQueue(){
 	Queue * queue = (Queue *) (malloc(sizeof(Queue)));
@@ -340,14 +502,7 @@ Node * Dequeue(Queue * queue){
 	queue->count--;
 	return rtn;
 }
-Node * GetNode (Queue * queue, void * data) {
-	if ( data == NULL ) return NULL;
+Node * Peek(Queue *queue){
 	if ( queue == NULL ) return NULL;
-	if ( !isempty(queue) ) return NULL;
-	Node * rtn = queue->front;
-	while ( rtn != NULL ) {
-		if ( rtn->data == data ) return rtn;
-		rtn = rtn->next;
-	}
-	return NULL;
+	return queue->front;
 }
